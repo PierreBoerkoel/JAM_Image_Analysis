31c05b479e59452a9270dfef9ebf6ebbadc34c08
PierreBoerkoel <pierreboerkoel@hotmail.com>
2021-02-17 16:39:58 -0800

    update graph formatting

--- r_codes/ch3_graphs_200623.R
+++ r_codes/ch3_graphs_200623.R
@@ -1,18 +1,18 @@
 remove(list=ls())
 
-mytablepath = '/Users/pierreboerkoel/Desktop/IBA-1'
-# param_name <- 'cy3norm'; myylim = c(0,4); myylabel <- 'Layer-wise normalized % Aβ immunoreactivity'
-# param_name <- 'fitcnorm'; myylim = c(0,2); myylabel <- 'Layer-wise normalized % IBA-1 immunoreactivity'
-# param_name <- 'cy3_and_fitc'; myylim = c(0,0.20); myylabel <- 'Layer-wise normalized % Aβ and IBA-1 immunoreactivity'
-param_name <- 'cy3per_fitc'; myylim = c(0,70); myylabel <- 'Layer-wise % Aβ immunoreactivity in an IBA-1 immunoreactive area'
+mytablepath = '/Users/pierreboerkoel/Desktop/AD Project'
+param_name <- 'cy3norm'; myylim = c(0,3); myylabel <- 'Layer-wise normalized % Aβ immunoreactivity'
+# param_name <- 'fitcnorm'; myylim = c(0,4); myylabel <- 'Layer-wise normalized % IBA-1 immunoreactivity'
+# param_name <- 'cy3_and_fitc'; myylim = c(0,0.3); myylabel <- 'Layer-wise normalized % Aβ and IBA-1 immunoreactivity'
+# param_name <- 'cy3per_fitc'; myylim = c(0,60); myylabel <- 'Layer-wise % Aβ immunoreactivity in a IBA-1 immunoreactive area'
 
 # param_name <- 'cy3per_fitcn'
 # param_name <- 'cy3per_fitcratio'
 # param_name <- 'thickum'; myylim = c(0,85); myylabel <- 'thickness (um)'
 
-labeltable <- read.csv(paste0(mytablepath,'/ALL_g_labeltable.csv'))
+labeltable <- read.csv(paste0(mytablepath,'/IBA-1_Data/ALL_IBA-1_labeltable.csv'))
 colnames(labeltable) <- c('Label','Subject','Dx','Region')
-param <- read.csv(paste0(mytablepath,'/ALL_g_cy3perfitc.csv'))
+param <- read.csv(paste0(mytablepath,'/IBA-1_Data/ALL_IBA-1_',param_name,'.csv'))
 colnames(param) <- c('RNFL','GCL','IPL','INL','OPL','ONL')
 data_wide = cbind(labeltable,param)
 
@@ -29,23 +29,34 @@ library(reshape2)
 data_clean <- melt(data_wide_clean, measure.vars = 5:10)
 data_clean <- data_clean[!data_clean$value=='NA',]
 data_clean <- data_clean[!data_clean$value=='NaN',]
+names(data_clean)[3]<-"Diagnosis"
 names(data_clean)[5]<-"Layer"  
 names(data_clean)[6]<-"Value" # Long format
 data_clean$Value <- as.numeric(data_clean$Value)
 data <- data_clean
+data$Diagnosis <- gsub("Normal", "Control", data$Diagnosis)
 
 # Plot
 library(ggpubr)
 #install.packages("viridis")  # Install
-library("viridis")     
+library("viridis")
+#, width = 850, height = 850, res = 80
 
-png(filename = paste0(mytablepath,'/IBA-1 Results/BarPlots/',param_name,'_IBA-1.png'))
+png(filename = paste0(mytablepath,'/IBA-1 Results/',param_name,'_IBA-1_updated_layout_outline_default.png'))
 
 #p <- ggboxplot(data[data$Label=='IBA-1',], x = "Layer", y="Value", order= c('RNFL','GCL','IPL','INL','OPL','ONL'), color="Dx",
 #               facet.by = "Region", labelOutliers='TRUE',add="jitter", ylim=myylim, ylab=myylabel, outlier.shape=8)
-p <- ggbarplot(data[data$Label=='IBA-1',], x = "Layer", y="Value", order= c('RNFL','GCL','IPL','INL','OPL','ONL'),
-               color="Dx", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
-               ylim=myylim, add = c("mean_se"), palette="Set1")
-p + stat_compare_means(aes(group = Dx), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
+p <- ggbarplot(data[data$Label=='IBA-1',], x = "Layer", y="Value", order = c('RNFL','GCL','IPL','INL','OPL','ONL'),
+               color="Diagnosis", facet.by = "Region", panel.labs = list(Region = c("Central", "Peripheral")), ylab=myylabel, lab.size = "100", position = position_dodge(0.9),
+               ylim=myylim, add = c("mean_se"), palette="Set1", size = 0.7)
+p + stat_compare_means(aes(group = Diagnosis), method = "wilcox.test", label = "p.signif", hide.ns=TRUE, label.y=c(myylim[2]*0.99))+
+  font("ylab", size = 16)+
+  font("xlab", size = 16)+
+  font("xy.text", size = 14)+
+  theme(strip.text.x = element_text(size = 14))+
+  guides(color = guide_legend(title = "",
+                              label = TRUE))
+
+#  ggsave(paste0(mytablepath,'/IBA-1 Results/',param_name,'_IBA-1_updated_layout.tiff'), p, device = "tiff", dpi = 400)
 
 dev.off()
--- r_codes/ch3_graphs_colocalization_for_pierre_200825.R
+++ r_codes/ch3_graphs_colocalization_for_pierre_200825.R
@@ -1,43 +1,43 @@
 remove(list=ls())
 
-mytablepath = 'C:/Users/Sieun/Desktop/Ch3/Measurements'
+mytablepath = '/Users/pierreboerkoel/Desktop/AD Project'
 
 # load both fitc and fitcn
 param_name <- 'cy3per_fitc'; myylim = c(0,55); myylabel <- 'cy3 colocalized with fitc (%)'
-labeltable <- read.csv(paste0(mytablepath,'/ALL_labeltable.csv'))
+labeltable <- read.csv(paste0(mytablepath,'/GFAP_TUBB_Data/ALL_GFAP_TUBB_labeltable.csv'))
 colnames(labeltable) <- c('Label','Subject','Dx','Region')
-param <- read.csv(paste0(mytablepath,'/ALL_',param_name,'.csv'))
+param <- read.csv(paste0(mytablepath,'/GFAP_TUBB_Data/ALL_GFAP_TUBB_',param_name,'.csv'))
 colnames(param) <- c('RNFL','GCL','IPL','INL','OPL','ONL')
 clcol <- matrix(rep('fitc', each=nrow(param)), nrow=nrow(param)); colnames(clcol) <- 'cy3_colocalization'
 data_wide1 = cbind(labeltable,clcol,param); 
 
 # Remove Outliers
-source('C:/Users/Sieun/Desktop/Ch3/removeOutliers_ch3.R')
-data_wide_clean1 <- removeOutliers_ch3(data_wide1,'GFAP','Normal','C'); 
-mytmp <- removeOutliers_ch3(data_wide1,'GFAP','Normal','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide1,'GFAP','AD','C'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide1,'GFAP','AD','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide1,'TUBB','Normal','C'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
+source('/Users/pierreboerkoel/git/ADImageAnalysis/JAM_Image_Analysis/r_codes/removeOutliers_ch3.R')
+data_wide_clean1 <- removeOutliers_ch3(data_wide1,'TUBB','Normal','C'); 
 mytmp <- removeOutliers_ch3(data_wide1,'TUBB','Normal','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide1,'TUBB','AD','C'); data_wide_clean1 = rbind(data_wide_clean1, mytmp);
-mytmp <- removeOutliers_ch3(data_wide1,'TUBB','AD','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp);
+mytmp <- removeOutliers_ch3(data_wide1,'TUBB','AD','C'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
+mytmp <- removeOutliers_ch3(data_wide1,'TUBB','AD','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
+# mytmp <- removeOutliers_ch3(data_wide1,'TUBB','Normal','C'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
+# mytmp <- removeOutliers_ch3(data_wide1,'TUBB','Normal','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp); 
+# mytmp <- removeOutliers_ch3(data_wide1,'TUBB','AD','C'); data_wide_clean1 = rbind(data_wide_clean1, mytmp);
+# mytmp <- removeOutliers_ch3(data_wide1,'TUBB','AD','P'); data_wide_clean1 = rbind(data_wide_clean1, mytmp);
 
 param_name2 <- 'cy3per_fitcn';
-param2 <- read.csv(paste0(mytablepath,'/ALL_',param_name2,'.csv'))
+param2 <- read.csv(paste0(mytablepath,'/GFAP_TUBB_Data/ALL_GFAP_TUBB_',param_name2,'.csv'))
 colnames(param2) <- c('RNFL','GCL','IPL','INL','OPL','ONL')
 clcol2 <- matrix(rep('fitcn', each=nrow(param2)), nrow=nrow(param2)); colnames(clcol2) <- 'cy3_colocalization'
 data_wide2 = cbind(labeltable,clcol2,param2); 
 
 # Remove Outliers
-source('C:/Users/Sieun/Desktop/Ch3/removeOutliers_ch3.R')
-data_wide_clean2 <- removeOutliers_ch3(data_wide2,'GFAP','Normal','C'); 
-mytmp <- removeOutliers_ch3(data_wide2,'GFAP','Normal','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide2,'GFAP','AD','C'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide2,'GFAP','AD','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide2,'TUBB','Normal','C'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
+source('/Users/pierreboerkoel/git/ADImageAnalysis/JAM_Image_Analysis/r_codes/removeOutliers_ch3.R')
+data_wide_clean2 <- removeOutliers_ch3(data_wide2,'TUBB','Normal','C'); 
 mytmp <- removeOutliers_ch3(data_wide2,'TUBB','Normal','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
-mytmp <- removeOutliers_ch3(data_wide2,'TUBB','AD','C'); data_wide_clean2 = rbind(data_wide_clean2, mytmp);
-mytmp <- removeOutliers_ch3(data_wide2,'TUBB','AD','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp);
+mytmp <- removeOutliers_ch3(data_wide2,'TUBB','AD','C'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
+mytmp <- removeOutliers_ch3(data_wide2,'TUBB','AD','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
+# mytmp <- removeOutliers_ch3(data_wide2,'TUBB','Normal','C'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
+# mytmp <- removeOutliers_ch3(data_wide2,'TUBB','Normal','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp); 
+# mytmp <- removeOutliers_ch3(data_wide2,'TUBB','AD','C'); data_wide_clean2 = rbind(data_wide_clean2, mytmp);
+# mytmp <- removeOutliers_ch3(data_wide2,'TUBB','AD','P'); data_wide_clean2 = rbind(data_wide_clean2, mytmp);
 
 data_wide_clean <- rbind(data_wide_clean1, data_wide_clean2)
 
@@ -56,42 +56,42 @@ data <- data_clean
 library(ggpubr)
 #install.packages("viridis")  # Install
 
-png(filename = paste0(mytablepath,'/coloc_GFAP_Normal.png')) 
-
-p <- ggbarplot(data[data$Label=='GFAP'&data$Dx=='Normal',], x = "Layer", y="Value", 
-               order= c('RNFL','GCL','IPL','INL','OPL','ONL'), 
-               color="cy3_colocalization", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
-               ylim=myylim, add = c("mean_se"), palette="Set1")
-p + stat_compare_means(aes(group = cy3_colocalization), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
-
-dev.off()
-
-png(filename = paste0(mytablepath,'/coloc_GFAP_AD.png')) 
-
-p <- ggbarplot(data[data$Label=='GFAP'&data$Dx=='AD',], x = "Layer", y="Value", 
-               order= c('RNFL','GCL','IPL','INL','OPL','ONL'), 
-               color="cy3_colocalization", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
-               ylim=myylim, add = c("mean_se"), palette="Set1")
-p + stat_compare_means(aes(group = cy3_colocalization), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
-
-dev.off()
-
-png(filename = paste0(mytablepath,'/coloc_TUBB_Normal.png')) 
+png(filename = paste0(mytablepath,'/TUBB Results/','coloc_TUBB_Normal.png')) 
 
 p <- ggbarplot(data[data$Label=='TUBB'&data$Dx=='Normal',], x = "Layer", y="Value", 
                order= c('RNFL','GCL','IPL','INL','OPL','ONL'), 
                color="cy3_colocalization", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
-               ylim=myylim, add = c("mean_se"), palette="Set1")
+               ylim=myylim, add = c("mean_se"), palette = c("#16C416", "#000000"))
 p + stat_compare_means(aes(group = cy3_colocalization), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
 
 dev.off()
 
-png(filename = paste0(mytablepath,'/coloc_TUBB_AD.png')) 
+png(filename = paste0(mytablepath,'/TUBB Results/','coloc_TUBB_AD.png')) 
 
 p <- ggbarplot(data[data$Label=='TUBB'&data$Dx=='AD',], x = "Layer", y="Value", 
                order= c('RNFL','GCL','IPL','INL','OPL','ONL'), 
                color="cy3_colocalization", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
-               ylim=myylim, add = c("mean_se"), palette="Set1")
+               ylim=myylim, add = c("mean_se"), palette = c("#16C416", "#000000"))
 p + stat_compare_means(aes(group = cy3_colocalization), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
 
 dev.off()
+
+# png(filename = paste0(mytablepath,'/coloc_TUBB_Normal.png')) 
+# 
+# p <- ggbarplot(data[data$Label=='TUBB'&data$Dx=='Normal',], x = "Layer", y="Value", 
+#                order= c('RNFL','GCL','IPL','INL','OPL','ONL'), 
+#                color="cy3_colocalization", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
+#                ylim=myylim, add = c("mean_se"), palette="Set1")
+# p + stat_compare_means(aes(group = cy3_colocalization), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
+# 
+# dev.off()
+# 
+# png(filename = paste0(mytablepath,'/coloc_TUBB_AD.png')) 
+# 
+# p <- ggbarplot(data[data$Label=='TUBB'&data$Dx=='AD',], x = "Layer", y="Value", 
+#                order= c('RNFL','GCL','IPL','INL','OPL','ONL'), 
+#                color="cy3_colocalization", facet.by = "Region",  ylab=myylabel, position = position_dodge(0.8),
+#                ylim=myylim, add = c("mean_se"), palette="Set1")
+# p + stat_compare_means(aes(group = cy3_colocalization), method = "wilcox.test", label = "p.signif", label.y=c(myylim[2]*0.99))
+# 
+# dev.off()
